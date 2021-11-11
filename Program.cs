709d1896a892f1267ae6e472419d790d6f4c6729
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2021-11-11 06:42:11 +0100

    Yer another universe added

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp7
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Universes--!");
+            Console.WriteLine("Hello yet another universe!");
         }
     }
 }
